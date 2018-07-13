6a59aec7ccdf40b04cbf82114d26db59bbd00e12
Jonathan MacKenzie <jmail97@gmail.com>
2018-07-13 17:04:32 -0300

    remove init script, as it can be contained within full dotfile script

--- .init-dotfiles.sh
+++
@@ -1,9 +0,0 @@
-# install oh-my-zsh, keeping the .zshrc from the repo and replacing the auto-generated one with it
-mv .zshrc newzshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-mv newzshrc .zshrc
-
-# bring in all the submodules (mostly vim plugins)
-config submodule init;
-config submodule update;
-
